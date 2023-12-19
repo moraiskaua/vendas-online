7fe2126eb67c189829ec2001dc6f0c2d8f4582e7
moraiskaua <moraiskaua03@gmail.com>
2023-12-19 15:08:11 -0300

    feat: create login modules

---
+++ mobile/src/modules/login/screens/Login.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+
+interface LoginProps {}
+
+const Login = ({}: LoginProps) => {
+  return (
+    <View>
+      <Text>Tela de Login!</Text>
+    </View>
+  );
+};
+
+export default Login;
